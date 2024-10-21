e1d275cbafb2f2a0a060d6777bb11810fd632d36
reamfox <sashafarion@gmail.com>
2024-10-21 15:45:37 +0300

    innit commit

---
+++ app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/tms/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.weatherapp.freeweather.free.app.mobile.androidapp.tms
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.weatherapp.freeweather.free.app.mobile.androidapp.tms.ui.theme.TMSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TMSTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TMSTheme {
+        Greeting("Android")
+    }
+}
